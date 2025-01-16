5eb511b90a6450ff2bc74ff6aa197356708f0530
Enrico0418 <enrico.marchisio2@mail.dcu.ie>
2025-01-16 15:36:37 +0000

    Update density vs cross section map.py
    Automated calculation of Radius, Area and MFP incorporate

--- density vs cross section map.py
+++ density vs cross section map.py
@@ -13,59 +13,126 @@ from matplotlib.colors import LogNorm
 from scipy.optimize import curve_fit
 from scipy import stats    
 
-dataset=np.loadtxt('Europa_data.csv', delimiter=',').T
+datasetE = np.loadtxt('Europa_data.csv', delimiter=',').T
+datasetI = np.loadtxt('Io_data.csv', delimiter=',').T
+datasetC = np.loadtxt('Callisto_data.csv', delimiter=',').T
 
-mass = dataset[0]
-density = dataset[1]
-ndensity = dataset[2]
-R = []
-CSA = []
-MFP = []
+massE = datasetE[0]
+densityE = datasetE[1]
+ndensityE = datasetE[2]
+RE = []
+CSAE = []
+MFPE = []
 
-for i in range(0, len(ndensity)):
-    for j in range(0, len(mass)):
-        for k in range(0, len(density)):
-            r = ((3*mass[j])/(4*3.14*density[k]))**(1/3)
-            R.append(r)
+massI = datasetI[0]
+densityI = datasetI[1]
+ndensityI = datasetI[2]
+RI = []
+CSAI = []
+MFPI = []
+
+massC = datasetC[0]
+densityC = datasetC[1]
+ndensityC = datasetC[2]
+RC = []
+CSAC = []
+MFPC = []
+
+for i in range(0, len(ndensityE)):
+    for j in range(0, len(massE)):
+        for k in range(0, len(densityE)):
+            r = ((3*massE[j])/(4*3.14*densityE[k]))**(1/3)
+            RE.append(r)
             csa = 4*3.14*(r**2)
-            CSA.append(csa)
-            mfp = 1/(ndensity[i]*csa)
-            MFP.append(mfp)
+            CSAE.append(csa)
+            mfp = 1/(ndensityE[i]*csa)
+            MFPE.append(mfp)
 
-R = np.array(R)
-CSA = np.array(CSA)
-MFP = np.array(MFP)
-ndensity = np.array(ndensity)
-print(R)
-print(CSA)
-print(MFP)
+RE = np.array(RE)
+CSAE = np.array(CSAE)
+MFPE = np.array(MFPE)
+ndensityE = np.array(ndensityE)
 
-X_E = CSA   
+print('Europa data')
+print('Cross Section area of dust:', CSAE)
+print('Mean Free Path', MFPE)
+
+
+for i in range(0, len(ndensityI)):
+    for j in range(0, len(massI)):
+        for k in range(0, len(densityI)):
+            r = ((3*massI[j])/(4*3.14*densityI[k]))**(1/3)
+            RI.append(r)
+            csa = 4*3.14*(r**2)
+            CSAI.append(csa)
+            mfp = 1/(ndensityE[i]*csa)
+            MFPI.append(mfp)
+
+RI = np.array(RI)
+CSAI = np.array(CSAI)
+MFPI = np.array(MFPI)
+ndensityI = np.array(ndensityI)
+
+print('Io data')
+print('Cross Section area of dust:', CSAI)
+print('Mean Free Path', MFPI)
+
+for i in range(0, len(ndensityC)):
+    for j in range(0, len(massC)):
+        for k in range(0, len(densityC)):
+            r = ((3*massC[j])/(4*3.14*densityC[k]))**(1/3)
+            RC.append(r)
+            csa = 4*3.14*(r**2)
+            CSAC.append(csa)
+            mfp = 1/(ndensityE[i]*csa)
+            MFPC.append(mfp)
+
+RC = np.array(RC)
+CSAC = np.array(CSAC)
+MFPC = np.array(MFPC)
+ndensityC = np.array(ndensityC)
+
+print('Callisto data')
+print('Cross Section area of dust:', CSAC)
+print('Mean Free Path', MFPC)
+
+X_E = CSAE 
+X_I = CSAI
+X_C = CSAC
 Y_E = []
+Y_I = []
+Y_C = []
 
 for m in range(0, 9):
-    Y_E.append(ndensity[0])
-print(Y_E)
+    Y_E.append(ndensityE[0])
 for m in range(9, 18):
-    Y_E.append(ndensity[1])
+    Y_E.append(ndensityE[1])
 for m in range(18, 27):
-    Y_E.append(ndensity[2])
+    Y_E.append(ndensityE[2])
 
+for m in range(0, 4):
+    Y_I.append(ndensityI[0])
+for m in range(4, 8):
+    Y_I.append(ndensityI[1])
 
-
-
-plt.figure()            
-plt.xlabel("Cross-section area (cm^2)")    
-plt.ylabel("Number density (m^-3")   
+for m in range(0, 9):
+    Y_C.append(ndensityC[0])
+for m in range(9, 18):
+    Y_C.append(ndensityC[1])
+for m in range(18, 27):
+    Y_C.append(ndensityC[2])
+    
 
 fig, ax = plt.subplots()
 ax.scatter(X_E, Y_E, label='Europa data', marker='s', color='b')
+ax.scatter(X_I, Y_I, label='Io data', marker='.', color='y')
+ax.scatter(X_C, Y_C, label='Callisto data', marker='^', color='r')
 
-ax.set_xlim(1e-11, 1e-10)
+ax.set_xlim(1e-12, 1e-10)
 ax.set_ylim(1e-9, 0.0012)
 
+plt.xlabel("Cross-section area (cm^2)")    
+plt.ylabel("Number density (m^-3")   
 
-plt.ylabel('Number density (m^-3)')
-plt.xlabel(' Cross section (cm^2)')
 
 plt.legend()
